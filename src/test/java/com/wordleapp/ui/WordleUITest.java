4017c7cd312d5b4403d000c96afe230a4e0cdecd
miguel.serralta <miguel.serralta@securitasdirect.es>
2025-02-19 02:43:34 +0100

    fixing driver options

--- src/test/java/com/wordleapp/ui/WordleUITest.java
+++ src/test/java/com/wordleapp/ui/WordleUITest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,7 +22,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
     @BeforeAll
     void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:8080/");
     }
 
