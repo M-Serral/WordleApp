4d46db4d950cf4cb793aa45e5940ad7577e85c9a
miguel.serralta <miguel.serralta@securitasdirect.es>
2025-03-04 04:33:24 +0100

    Feature/v0.2.1 adding MockMVC tests without mockbean v2

--- src/test/java/com/wordleapp/integration/WordleControllerTest.java
+++ src/test/java/com/wordleapp/integration/WordleControllerTest.java
@@ -1,18 +1,13 @@
 package com.wordleapp.integration;
 
-import com.wordleapp.controller.WordleController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.server.ResponseStatusException;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -39,4 +34,19 @@ public class WordleControllerTest {
                 .andExpect(content().string(expectedMessage));
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "WRONa, Try again! Attempts left: 5",
+            "WRONb, Try again! Attempts left: 4",
+            "WRONc, Try again! Attempts left: 3",
+            "WRONd, Try again! Attempts left: 2",
+            "WRONe, Try again! Attempts left: 1",
+    })
+    public void testUserAttemptsAndFails(String guess, String expectedMessage) throws Exception {
+        mockMvc.perform(post("/api/wordle/guess")
+                        .param("guess", guess)
+                        .param("user", "testUserFails"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedMessage));
+    }
 }
