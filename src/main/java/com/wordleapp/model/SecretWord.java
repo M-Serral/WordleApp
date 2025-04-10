e06b1b2ab8016eafbf028e178b5965f88aa1ad06
miguel.serralta <miguel.serralta@securitasdirect.es>
2025-04-10 14:23:30 +0200

    feat: create SecretWord entity

---
+++ src/main/java/com/wordleapp/model/SecretWord.java
@@ -0,0 +1,28 @@
+package com.wordleapp.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Entity
+public class SecretWord {
+
+    // Getters & Setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, unique = true, length = 5)
+    private String word;
+
+    // Constructors
+    public SecretWord() {
+    }
+
+    public SecretWord(String word) {
+        this.word = word;
+    }
+
+}
