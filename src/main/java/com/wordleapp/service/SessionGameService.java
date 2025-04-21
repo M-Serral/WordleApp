74e0f724e91d8c8f66023de96426314ab2b6ad89
miguel.serralta <miguel.serralta@securitasdirect.es>
2025-04-21 04:48:32 +0200

    refactor(session): persist username and word in session on game reset

--- src/main/java/com/wordleapp/service/SessionGameService.java
+++ src/main/java/com/wordleapp/service/SessionGameService.java
@@ -4,6 +4,8 @@ import com.wordleapp.utils.*;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class SessionGameService {
 
@@ -13,10 +15,16 @@ public class SessionGameService {
         this.wordSelectorService = wordSelectorService;
     }
 
-    public void resetGame(HttpSession session) {
+    public void resetGame(String username, HttpSession session) {
         session.removeAttribute(Constants.ATTEMPTS_KEY);
         session.removeAttribute(Constants.GAME_WON_KEY);
         session.removeAttribute(Constants.LAST_HINT_KEY);
         wordSelectorService.selectRandomWord();
+        session.setAttribute(Constants.SECRET_WORD_KEY, wordSelectorService.getCurrentWord());
+        session.setAttribute(Constants.USERNAME_KEY, username);
+        // Generate new server session identifier
+        if (session.getAttribute(Constants.SESSION_ID) == null) {
+            session.setAttribute(Constants.SESSION_ID, UUID.randomUUID().toString());
+        }
     }
 }
