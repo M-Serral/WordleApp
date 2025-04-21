97a032c6ab099774503d92d65139d9ba189fbea9
miguel.serralta <miguel.serralta@securitasdirect.es>
2025-04-21 04:49:25 +0200

    chore(repository): create JPA repository for Game entity

---
+++ src/main/java/com/wordleapp/repository/GameRepository.java
@@ -0,0 +1,7 @@
+package com.wordleapp.repository;
+
+import com.wordleapp.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
