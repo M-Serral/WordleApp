2c3df2ef70c7f14ebfbe5983b5ccef86b4bf528e
miguel.serralta <miguel.serralta@securitasdirect.es>
2025-05-07 23:55:14 +0200

    test: updating RankingServiceTest with values that cover all functionality

--- src/test/java/com/wordleapp/integration/RankingServiceTest.java
+++ src/test/java/com/wordleapp/integration/RankingServiceTest.java
@@ -35,23 +35,53 @@ class RankingServiceTest extends BaseTestConfiguration {
 
     @BeforeEach
     void setUp() {
-        SecretWord word = new SecretWord("SEXTO");
-        secretWordRepository.save(word);
+        SecretWord secretWord = new SecretWord("LIBRO");
+        secretWordRepository.save(secretWord);
 
-        gameRepository.save(new Game("Ana", 2, word, LocalDateTime.now()));
-        gameRepository.save(new Game("Luis", 1, word, LocalDateTime.now()));
-        gameRepository.save(new Game("Eva", 3, word, LocalDateTime.now()));
-        gameRepository.save(new Game("Javi", 1, word, LocalDateTime.now())); // draw with Luis
+        gameRepository.save(new Game("User1", 1, secretWord, LocalDateTime.now()));
+        gameRepository.save(new Game("User2", 3, secretWord, LocalDateTime.now().minusMinutes(10)));
+        gameRepository.save(new Game("User3", 1, secretWord, LocalDateTime.now().minusMinutes(20)));
+        gameRepository.save(new Game("User4", 2, secretWord, LocalDateTime.now().minusMinutes(30)));
+        gameRepository.save(new Game("User5", 4, secretWord, LocalDateTime.now().minusMinutes(40)));
+        gameRepository.save(new Game("User6", 2, secretWord, LocalDateTime.now().minusMinutes(50)));
+        gameRepository.save(new Game("User7", 5, secretWord, LocalDateTime.now().minusMinutes(60)));
+        gameRepository.save(new Game("User8", 3, secretWord, LocalDateTime.now().minusMinutes(70)));
+
+
+
+    }
+
+    @Test
+    void shouldReturnTop1IncludingTies() {
+        List<Game> rankings = rankingService.findTopRankingsBySecretWord("LIBRO", 1);
+
+        assertEquals(2, rankings.size()); // tie in first position
+        assertEquals("User1", rankings.get(0).getUsername());
+        assertEquals("User3", rankings.get(1).getUsername());
     }
 
     @Test
     void shouldReturnTop3IncludingTies() {
-        List<Game> rankings = rankingService.findTopRankingsBySecretWord("SEXTO", 3);
+        List<Game> rankings = rankingService.findTopRankingsBySecretWord("LIBRO", 3);
 
-        assertEquals(3, rankings.size()); // Luis, Javi, Ana
-        assertEquals("Luis", rankings.get(0).getUsername());
-        assertEquals("Javi", rankings.get(1).getUsername());
-        assertEquals("Ana", rankings.get(2).getUsername());
+        assertEquals(4, rankings.size()); // 2 ties, in first position and in second position
+        assertEquals("User1", rankings.get(0).getUsername());
+        assertEquals("User3", rankings.get(1).getUsername());
+        assertEquals("User4", rankings.get(2).getUsername());
+        assertEquals("User6", rankings.get(3).getUsername());
+    }
+
+    @Test
+    void shouldReturnTop5IncludingTies() {
+        List<Game> rankings = rankingService.findTopRankingsBySecretWord("LIBRO", 5);
+
+        assertEquals(6, rankings.size()); // 3 ties, first,second,third position
+        assertEquals("User1", rankings.get(0).getUsername());
+        assertEquals("User3", rankings.get(1).getUsername());
+        assertEquals("User4", rankings.get(2).getUsername());
+        assertEquals("User6", rankings.get(3).getUsername());
+        assertEquals("User2", rankings.get(4).getUsername());
+        assertEquals("User8", rankings.get(5).getUsername());
     }
 
     @Test
